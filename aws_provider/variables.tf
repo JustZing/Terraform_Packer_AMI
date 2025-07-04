802c0508fc379d38e6c4c8763d89601820e4cd79
Nzinga <nzingabediako@yahoo.com>
2025-07-03 21:49:02 -0400

    Trigger GitHub Actions workflow run

--- aws_provider/variables.tf
+++
@@ -1,9 +0,0 @@
-# modules/aws_provider/variables.tf
-# Defines the variables for the AWS provider module
-
-variable "region" {
-  description = "AWS region to use"
-  type        = string
-  default     = "us-east-1"  # Default region, but it can be overridden from the root module
-}
-
--- providers.tf
+++
@@ -1,7 +0,0 @@
-# modules/aws_provider/main.tf
-# Defines the AWS provider configuration
-
-provider "aws" {
-  region = var.region  # Using the region passed from the root module
-}
-
